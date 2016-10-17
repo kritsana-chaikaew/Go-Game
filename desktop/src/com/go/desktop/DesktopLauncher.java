16aa8c4215895560a828d7feb48c11895a496e4d
Kritsana Chaikaew <kritsana.chai@ku.th>
2016-10-17 10:01:30 +0700

    Initial project

---
+++ desktop/src/com/go/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.go.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.go.GoGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GoGame(), config);
+	}
+}
