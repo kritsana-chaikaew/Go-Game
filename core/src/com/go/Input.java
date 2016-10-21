b21d6e0f814da821de800012898a756812622d8e
Kritsana Chaikaew <kritsana.chai@ku.th>
2016-10-21 22:46:37 +0700

    Add Input basic placing troop, need cleaning

--- core/src/com/go/Input.java
+++ core/src/com/go/Input.java
@@ -9,6 +9,7 @@ public class Input extends InputAdapter {
   Panel panel;
 
   Troop troop = Troop.EMPTY_TROOP;
+  Stone stone = Stone.EMPTY_STONE;
 
   public Input (Board board, Panel panel) {
     this.board = board;
@@ -21,8 +22,6 @@ public class Input extends InputAdapter {
   public boolean touchDown (int x, int y, int pointer, int button) {
     int row = x / Block.BLOCK_SIZE;
     int column = y / Block.BLOCK_SIZE;
-
-    System.out.println(row + "hello" + column);
 
     onClick(row, column);
 
@@ -38,19 +37,38 @@ public class Input extends InputAdapter {
     }
   }
 
-  public void setTroopOnClick (Troop troop, int row, int column) {
+  public Stone getStoneOnclick (int row, int column) {
+    if (row < Panel.PANEL_WIDHT && column < Panel.PANEL_HEIGHT) {
+      return panel.getStoneBlock(row, column).getStoneLayer();
+    } else {
+      return Stone.EMPTY_STONE;
+    }
+  }
 
-    board.troopBlocks[row][column].setTroopLayer(troop);
+  public void setTroopOnClick (Troop troop, int row, int column) {
+    if (board.troopBlocks[row][column].getTroopLayer() == Troop.EMPTY_TROOP){
+      board.troopBlocks[row][column].setTroopLayer(troop);
+    }
     System.out.println("setTroop");
+  }
+
+  public void setStoneOnClick (Stone stone, int row, int column) {
+    if (board.stoneBlocks[row][column].getStoneLayer() == Stone.EMPTY_STONE){
+      board.stoneBlocks[row][column].setStoneLayer(stone);
+    }
+    System.out.println("setStone");
   }
 
   public void onClick (int row, int column) {
     if (row < Board.BOARD_SIZE && column < Board.BOARD_SIZE) {
       setTroopOnClick(troop, row, column);
+      setStoneOnClick(stone, row, column);
       troop = Troop.EMPTY_TROOP;
+      stone = Stone.EMPTY_STONE;
     } else if (row >= Board.BOARD_SIZE || column >= Board.BOARD_SIZE) {
       System.out.println("getTroop");
       troop = getTroopOnClick(row - Board.BOARD_SIZE, column);
+      stone = getStoneOnclick(row - Board.BOARD_SIZE, column);
     }
   }
 }
--- core/src/com/go/WorldRenderer.java
+++ core/src/com/go/WorldRenderer.java
@@ -1,5 +1,8 @@
 package com.go;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+
 public class WorldRenderer {
   GoGame game;
   World world;
@@ -16,6 +19,29 @@ public class WorldRenderer {
 
   public void render () {
     boardRenderer.render();
+
+    if (world.input.stone == Stone.BLACK) {
+      drawBlock(Assets.blackStoneImage, Gdx.input.getX() / Block.BLOCK_SIZE * Block.BLOCK_SIZE,
+                (GoGame.SCREEN_HEIGHT - Gdx.input.getY() ) / Block.BLOCK_SIZE * Block.BLOCK_SIZE);
+    } else if (world.input.stone == Stone.WHITE) {
+      drawBlock(Assets.whiteStoneImage, Gdx.input.getX() / Block.BLOCK_SIZE * Block.BLOCK_SIZE,
+                (GoGame.SCREEN_HEIGHT - Gdx.input.getY() ) / Block.BLOCK_SIZE * Block.BLOCK_SIZE);
+    }
+
+    if (world.input.troop == Troop.WORKER) {
+      drawBlock(Assets.workerImage, Gdx.input.getX() / Block.BLOCK_SIZE * Block.BLOCK_SIZE,
+                (GoGame.SCREEN_HEIGHT - Gdx.input.getY() ) / Block.BLOCK_SIZE * Block.BLOCK_SIZE);
+    }
+
     panelRenderer.render();
   }
+
+  public void drawBlock (Texture image, int row, int column) {
+    if (image != null) {
+      game.batch.begin();
+      game.batch.draw(image, row, column);
+      game.batch.end();
+    }
+  }
+
 }
